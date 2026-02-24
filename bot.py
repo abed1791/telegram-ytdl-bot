4ce766359da77524bd93e552a5fc163503e50e21
abed1791 <96414935+abed1791@users.noreply.github.com>
2026-02-24 10:21:51 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,10 @@
+import os
+import yt_dlp
+from telegram.ext import ApplicationBuilder
+
+TOKEN = os.getenv("BOT_TOKEN")
+
+app = ApplicationBuilder().token(TOKEN).build()
 git add .
 git commit -m "update token to env"
 git push
